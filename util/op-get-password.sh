7caa4f4c18d51bf4b985106fe54448d909de5775
Ava Chaney <hello@ava.dev>
2023-05-10 22:13:35 -0700

    update vault password script

--- util/op-get-password.sh
+++ util/op-get-password.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# on the ansible management host, we get the vault password from a local file
+# this file is only accessible to root, so we need to use sudo to read it
+if [ -f "/opt/ansible/vault-password" ]; then
+    sudo cat /opt/ansible/vault-password && exit 0
+fi
+
 # Get the Ansible Vault password from 1Password vault
 op item get j2hjpgat7caqs37rknafoirmru --fields label=password
 if [ $? -ne 0 ]; then
